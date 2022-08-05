6bd697826210710e3ed10d38a77ba3ae654973f4
masakazu-hirano <105690400+masakazu-hirano@users.noreply.github.com>
2022-08-05 20:25:40 +0900

    04: Nested Components In Vue

---
+++ plan-picker.js
@@ -0,0 +1,19 @@
+Vue.createApp({
+})
+
+.component('plan-picker', {
+  template: '#plan-picker-template',
+  data() {
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict']
+    }
+  }
+})
+
+.component('plan', {
+  template: '#plan-template',
+  props: {
+    name: {type: String, required: true}
+  }
+  // props: ['name']
+}).mount('#app')
